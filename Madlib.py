3961a3e48958150a7ac235636c873abb9b1728a0
Kjersti Jacobson <kjersti.jacobson@gmail.com>
2016-09-14 17:37:39 -0600

    Adding madlib and gitignore

---
+++ Madlib.py
@@ -0,0 +1,16 @@
+adjective = raw_input("Can you give me an adjective, please? ")
+noun = raw_input("Can you think of a noun? ")
+number = raw_input("Hey! What's your lucky number? ")
+bodypart = raw_input("Got a favorite body part? ")
+noun2 = raw_input("I need a noun! " )
+verb = raw_input("Gimme a past tense verb ASAP! ")
+
+
+
+
+print """
+There once was a %s princess who lived in a %s.
+She was blessed with %s %ss and even more children.
+One day she tripped over the %s and broke all but one of her %ss.
+All of her children %s and no one lived happily ever after.
+""" % (adjective, noun, number, bodypart, noun2, bodypart, verb)
